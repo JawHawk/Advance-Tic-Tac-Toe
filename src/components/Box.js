10c02c727fc28e361ccaf3e5defd400aeea4334c
jawhawk <mavenal2143@gmail.com>
2022-12-08 14:23:21 +0530

    Styling done

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import circle from '../img/circle.png'
-import cross from '../img/cross.jpg'
+import cross from '../img/cross.png'
 import ResetContext from './resetContext'
 
 const initialState = {
-  sizes : ['40px','55px','75px','90px']
+  sizes : ['30px','45px','60px','75px']
 }
 
 class Box extends Component {
@@ -23,21 +23,27 @@ class Box extends Component {
     const {sizes} = this.state
     const {status, highlight ,size} = this.props
     const boxStyle = {
-        width:'15vw',
-        height:'15vw',
-        maxWidth:'90px',
-        maxHeight:'90px',
+        width:'15vw', height:'15vw',
+        maxWidth:'90px', maxHeight:'90px',
         border:'1px solid black',
         padding:'5px',
         cursor:'pointer',
         borderRadius:'10px',
+        background:'#F9CB86'
     }
     if(highlight){
       boxStyle['border'] = '2px solid blue'
     }
+    if(!status){
+      boxStyle['background'] = '#58a6bf'
+    }
+
     const imgStyle = {
         width: sizes[size],
-        heigh: sizes[size]
+        heigh: sizes[size],
+        position: 'relative',
+        top:'50%',
+        transform: 'translateY(-50%)'
     }
     return (
       <div style={boxStyle}>
